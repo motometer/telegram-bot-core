68a4dd534d52db27622f28d8e7c45723596eb0f8
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-12-14 11:31:55 +0200

    Redirect script output to /dev/null.

--- deploy-lambda.sh
+++ deploy-lambda.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-./gradlew clean build -x check
-aws lambda update-function-code --function-name motometer-bot --zip-file 'fileb://build/distributions/telegram-bot-core-0.0.1-SNAPSHOT.zip'
+./gradlew clean build -x check > /dev/null
+aws lambda update-function-code --function-name motometer-bot --zip-file 'fileb://build/distributions/telegram-bot-core-0.0.1-SNAPSHOT.zip' > /dev/null
+echo "Lambda uploaded successfully"
